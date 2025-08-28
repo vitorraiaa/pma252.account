a7062775cc1b6282dccf4d6f3b52582a585cedb9
Humberto Sandmann <hsandmann@ieee.org>
2025-08-28 14:38:35 -0300

    fixed body request

--- src/main/java/store/account/AccountController.java
+++ src/main/java/store/account/AccountController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface AccountController {
     
     @PostMapping("/account")
-    public AccountOut create(AccountIn in);
+    public AccountOut create(
+        @RequestBody AccountIn in
+    );
 
     @GetMapping("/account/{id}")
     public AccountOut findById(
