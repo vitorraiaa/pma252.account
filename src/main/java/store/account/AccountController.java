59e4ea649701346f01094d76ed074241ef888383
Humberto Sandmann <hsandmann@ieee.org>
2025-09-03 13:38:03 -0300

    Correction for response entity

--- src/main/java/store/account/AccountController.java
+++ src/main/java/store/account/AccountController.java
@@ -2,6 +2,7 @@ package store.account;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,20 +12,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface AccountController {
     
     @PostMapping("/account")
-    public AccountOut create(
+    public ResponseEntity<AccountOut> create(
         @RequestBody AccountIn in
     );
 
     @GetMapping("/account/{id}")
-    public AccountOut findById(
+    public ResponseEntity<AccountOut> findById(
         @PathVariable("id") String id
     );
 
     @GetMapping("/account")
-    public List<AccountOut> findAll();
+    public ResponseEntity<List<AccountOut>> findAll();
 
     @DeleteMapping("/account/{id}")
-    public Void delete(
+    public ResponseEntity<Void> delete(
         @PathVariable("id") String id
     );
 
