498ef826b347fcae1ad5ac3d87121f861163858b
Humberto Sandmann <hsandmann@ieee.org>
2025-09-05 15:53:08 -0300

    openfeing

--- src/main/java/store/account/AccountController.java
+++ src/main/java/store/account/AccountController.java
@@ -2,6 +2,7 @@ package store.account;
 
 import java.util.List;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@FeignClient(name = "account", url = "http://account:8080")
 public interface AccountController {
     
     @PostMapping("/account")
